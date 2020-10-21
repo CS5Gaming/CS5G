643b0752fdf8ad815c0fb0490106b67c1a126b69
CS5Gaming <CS5Gaming@github.com>
2020-10-21 21:12:24 +0600

    New

---
+++ Random Number Calculation C#/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prepare for your math exam: \n");
+            double randomNumber;
+            double randomNumber1;
+            double randomNumber2;
+            Random random = new Random();
+            randomNumber = random.Next(0,10);
+            randomNumber1 = random.Next(0,10);
+            randomNumber2= random.Next(0,10);
+
+            double result= randomNumber*randomNumber1/randomNumber2;
+            Console.WriteLine(+randomNumber+"*"+randomNumber1+"/"+randomNumber2);
+            double uresult = Convert.ToDouble( Console.ReadLine());
+            if (uresult==result){
+                Console.WriteLine(" Correct");
+            }
+            else
+            Console.WriteLine("Incorrect");
+
+            Console.ReadKey();
+        }
+    }
+}
