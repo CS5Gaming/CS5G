868b8a6d14b7615486d4e90cb75065890ac337e0
CS5Gaming <CS5Gaming@github.com>
2020-10-26 13:24:46 +0600

    New

---
+++ User Define Array Size C#/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NewArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many movies you want to store? ");
+
+            int n = Convert.ToInt32(Console.In.ReadLine());
+
+            string[] size = new string[n];
+  
+            for(int i = 0; i<n; i++){
+
+                 size[i] = Convert.ToString (Console.ReadLine());
+            }
+
+            Console.WriteLine("\nSort by Name: ");
+            Array.Sort(size);
+
+            for(int i=0; i<n; i++){
+
+                Console.WriteLine(size[i]);
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
