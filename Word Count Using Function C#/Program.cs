a329f871fe72be847d50724a0424da01c79953a1
CS5Gaming <CS5Gaming@github.com>
2020-10-28 12:33:26 +0600

    New

---
+++ Word Count Using Function C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wordNum = WordCount();
+            Console.WriteLine("There is " + wordNum + " words in the sentence");
+            Console.ReadKey();
+        }
+        static int WordCount()
+        {
+            int count = 1, lengt=0;
+            Console.WriteLine("Enter a sentence: ");
+            string array = Console.ReadLine();
+            while(lengt<=array.Length -1)
+            {
+                if(array[lengt]==' ' || array[lengt]=='\n' || array[lengt]=='\t')
+                {
+                    count++;
+                }
+                lengt++;
+            }
+            return count;
+        }
+    }
+}
